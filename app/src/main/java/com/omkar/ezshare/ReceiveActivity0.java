acfde73da5fe693eba47abc22dfcfc33b66c6039
omkar-sutar <omkartsutar2001@gmail.com>
2021-12-30 14:17:44 +0530

    File Reception fully functional. (File send yet to be implemented)

--- app/src/main/java/com/omkar/ezshare/ReceiveActivity0.java
+++ app/src/main/java/com/omkar/ezshare/ReceiveActivity0.java
@@ -1,29 +1,26 @@
 package com.omkar.ezshare;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.wifi.WifiManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
-
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class ReceiveActivity0 extends AppCompatActivity {
@@ -35,8 +32,6 @@ public class ReceiveActivity0 extends AppCompatActivity {
     private int port;
     private boolean socketCreatedFlag=false;
     private boolean cancel=false;
-    private String fileReceptionState="NotStarted";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,19 +87,29 @@ public class ReceiveActivity0 extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+                    //If user is not connected to wifi, ask to connect
+                    if(hostIP.equals("0.0.0.0")){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ReceiveActivity0.this);
+                        builder.setTitle("Wi-Fi unavailable")
+                                .setMessage("Please connect the device to a Wi-Fi network")
+                                .setPositiveButton("Ok", (dialogInterface, i) -> {
+                                    //Return from receive activity
+                                    finish();
+                                });
+                        AlertDialog alertDialog=builder.create();
+                        alertDialog.show();
+                        return;
+                    }
                     TextView textViewIP=findViewById(R.id.textViewIP);
                     textViewIP.setText(hostIP);
                     TextView textViewPort=findViewById(R.id.textViewPort);
                     textViewPort.setText(String.valueOf(port));
+
+                    TextView textViewReceptionInfo=findViewById(R.id.textViewReceptionStatus);
                     FileReceptionThread fileReceptionThread=new FileReceptionThread();
                     fileReceptionThread.start();
                 }
             });
-
-            //Now update the ui using UiHandler
-            //Message msg=new Message();
-            //msg.what=RECEIVE_SOCKET_CREATED_UPDATE_UI;
-            //UiHandler.sendMessage(msg);
         }
     }
 
@@ -117,28 +122,72 @@ public class ReceiveActivity0 extends AppCompatActivity {
               e.printStackTrace();
               return;
           }
-          String filename;
+          String filename,fileSize;
           byte[] buffer = new byte[1000000];
           int bytesReceived=0;
           File path=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
           try {
               DataInputStream dataInputStream=skr.getDataInputStream();
-              //get filename
               filename=dataInputStream.readUTF();
+              fileSize=dataInputStream.readUTF();   //in MB
               File file=new File(path,filename);
               try {
                   boolean res=file.createNewFile();
                   if(!res){
-
-                      return;
+                      //File already exists
+                      res=file.delete();
+                      if(!res){
+                          runOnUiThread(new Runnable() {
+                              @Override
+                              public void run() {
+                                  toast(getApplicationContext(),"The file already exists in the directory and couldn't be overwritten");
+                              }
+                          });
+                          skr.close();
+                          finish();
+                      }
                   }
               } catch (Exception e){
                   e.printStackTrace();
                   return;
               }
               FileOutputStream fileOutputStream=new FileOutputStream(file,false);
+              //update ui
+              runOnUiThread(new Runnable() {
+                  @SuppressLint("SetTextI18n")
+                  @Override
+                  public void run() {
+                      TextView textViewReceptionStatus=findViewById(R.id.textViewReceptionStatus);
+                      textViewReceptionStatus.setText("Receiving file: "+filename);
+                      ProgressBar progressBarCircle=findViewById(R.id.progressBarCircle);
+                      progressBarCircle.setVisibility(View.INVISIBLE);
+                      ProgressBar progressBarHorizontal=findViewById(R.id.progressBarHorizontal);
+                      progressBarHorizontal.setVisibility(View.VISIBLE);
+                      TextView textViewProgressBarPercentage = findViewById(R.id.textViewProgressBarPercentage);
+                      textViewProgressBarPercentage.setVisibility(View.VISIBLE);
+                      updateProgress(0,Float.parseFloat(fileSize));
+                  }
+              });
+              float megaBytesReceived=0;
+              float totalMegaBytes=Float.parseFloat(fileSize);
               while(true){
-                  bytesReceived=dataInputStream.read(buffer);
+                  try{
+                      bytesReceived=dataInputStream.read(buffer);
+                  } catch (IOException e){
+                      runOnUiThread(new Runnable() {
+                          @SuppressLint("SetTextI18n")
+                          @Override
+                          public void run() {
+                              TextView textViewReceptionStatus=findViewById(R.id.textViewReceptionStatus);
+                              textViewReceptionStatus.setText("Reception Failed");
+                              Button buttonCancel=findViewById(R.id.buttonCancelReceiveActivity0);
+                              buttonCancel.setText("Back");
+                          }
+                      });
+                      return;
+                  }
+                  megaBytesReceived+=((float)bytesReceived/1048576);
+                  updateProgress(megaBytesReceived,totalMegaBytes);
                   if(bytesReceived==-1) break;
                   else if(bytesReceived!=1000000){
                       fileOutputStream.write(Arrays.copyOf(buffer,bytesReceived));
@@ -149,6 +198,20 @@ public class ReceiveActivity0 extends AppCompatActivity {
                   }
               }
               fileOutputStream.close();
+              updateProgress(totalMegaBytes,totalMegaBytes);
+              runOnUiThread(new Runnable() {
+                  @SuppressLint("SetTextI18n")
+                  @Override
+                  public void run() {
+                      TextView textViewReceptionStatus=findViewById(R.id.textViewReceptionStatus);
+                      textViewReceptionStatus.setText("File Received: "+filename);
+                      TextView textViewReceivedFilePath=findViewById(R.id.textViewReceivedFilePath);
+                      textViewReceivedFilePath.setText("File saved at: "+path+"/"+filename);
+                      textViewReceivedFilePath.setVisibility(View.VISIBLE);
+                      Button buttonCancel=findViewById(R.id.buttonCancelReceiveActivity0);
+                      buttonCancel.setText("Back");
+                  }
+              });
               Log.i("mymsg",path.getPath());
               runOnUiThread(new Runnable() {
                   @Override
@@ -159,6 +222,20 @@ public class ReceiveActivity0 extends AppCompatActivity {
           } catch (Exception e) {
               e.printStackTrace();
           }
+      }
+      private void updateProgress(float megaBytesReceived,float totalMegaBytes){
+          runOnUiThread(new Runnable() {
+              @SuppressLint("SetTextI18n")
+              @Override
+              public void run() {
+                  int percentageReceived=(int)(megaBytesReceived*100/totalMegaBytes);
+                  String megabytesCount=String.valueOf((int)megaBytesReceived)+"/"+String.valueOf((int)totalMegaBytes);
+                  ProgressBar progressBarHorizontal=findViewById(R.id.progressBarHorizontal);
+                  progressBarHorizontal.setProgress((percentageReceived));
+                  TextView textViewProgressBarPercentage=findViewById(R.id.textViewProgressBarPercentage);
+                  textViewProgressBarPercentage.setText(String.valueOf(percentageReceived)+"% "+"(z MB)".replace("z",megabytesCount));
+              }
+          });
       }
     }
 }
